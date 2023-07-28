6271fadeb428933db3c48e4f98d2ec27328266b9
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-07-29 00:50:50 +0300

    Add blank start page

---
+++ where_to_go/where_to_go/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+
+
+def show_start_page(request):
+    return HttpResponse(
+        '''<!DOCTYPE html>
+        <html>
+        <head>
+          <title>Стартовая</title>
+        </head>
+        <body>
+          <h1>Здесь будет карта</h1>
+        </body>
+        </html>''')
