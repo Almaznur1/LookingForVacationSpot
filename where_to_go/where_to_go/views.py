4afa3cd668004fef4833a2a891d1eccd96d416e7
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-07-29 02:59:01 +0300

    Add frontend

--- where_to_go/where_to_go/views.py
+++ where_to_go/where_to_go/views.py
@@ -1,14 +1,6 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def show_start_page(request):
-    return HttpResponse(
-        '''<!DOCTYPE html>
-        <html>
-        <head>
-          <title>Стартовая</title>
-        </head>
-        <body>
-          <h1>Здесь будет карта</h1>
-        </body>
-        </html>''')
+    return render(request, 'index.html')
