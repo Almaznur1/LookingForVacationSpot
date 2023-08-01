38df7410fda6366b9f34af498174e661966cda61
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-08-01 07:02:21 +0300

    add logging to server.py

--- where_to_go/server.py
+++ where_to_go/server.py
@@ -1,5 +1,12 @@
 from waitress import serve
 from where_to_go.wsgi import application
+import logging
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 if __name__ == '__main__':
