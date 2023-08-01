ea7f3802c8b180681527c58a9cc597b3fa9cf14a
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-08-01 06:25:54 +0300

    add static urls to url_patterns

--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('', views.show_start_page),
     path('places/<int:place_id>/', views.show_place_json, name='place_json'),
     path('tinymce/', include('tinymce.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
