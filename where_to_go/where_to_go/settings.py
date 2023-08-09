51f895788b983cba49bef3da8834b610b5faa61b
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-08-09 21:09:39 +0300

    fix ALLOWED_HOSTS default value

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='127.0.0.1')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1'])
 
 WAITRESS_HOST = env.str('WAITRESS_HOST')
 # Application definition
